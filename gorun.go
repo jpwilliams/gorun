dcf33fbb5c5f4c610cf1bcdb906facbe4020f269
Jack Williams <jpwilliamsphotography@gmail.com>
2017-01-17 17:01:10 +0000

    Removing this for now. Will re-add attribution when in a more changed state

--- gorun.go
+++ gorun.go
@@ -1,14 +1,3 @@
-// Copyright 2013 The Authors. All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-//
-// Test Run: go run gorun.go
-// INSTALL: go install
-//
-// @Author: wangxian
-// @Created: 2013-07-31
-//
-// Project URL: https://github.com/wangxian/gorun
 package main
 
 import (
